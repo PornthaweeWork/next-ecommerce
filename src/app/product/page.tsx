25d5637daa605c47cf75ba7bbd14a4ac58ce959f
Pornthawee.t <pornthawee.work@gmail.com>
2024-03-15 21:50:21 +0700

    feat: add optionSort

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,63 @@
+"use client";
+import React, { useState, useEffect } from "react";
 
 export default function ProductPage() {
+  const [isShowSort, setIsShowSort] = useState(false);
+  const [optionSelected, setOptionSelected] = useState("ASC");
+  const optionSort: { value: string; label: string }[] = [
+    {
+      value: "ASC",
+      label: "Price - Low to high",
+    },
+    {
+      value: "DESC",
+      label: "Price - Low to high",
+    },
+    {
+      value: "ratings",
+      label: "Ratings",
+    },
+  ];
+
+  useEffect(() => {
+    console.log("isShowSort :>> ", isShowSort);
+  }, [isShowSort]);
+
   return (
-    <>
-      <div>Product List</div>
-    </>
-  )
+    <div className="my-[100px] flex">
+      <div className="mr-[130px] w-[280px]">accdiron</div>
+      <div className="flex flex-1">
+        <div className="flex w-full items-center justify-between">
+          <span className="self-start text-2xl font-bold">
+            Woman’s Clothing
+          </span>
+          <div className="relative flex w-fit flex-col items-end justify-center">
+            <button
+              className="border p-3"
+              onClick={(): void => setIsShowSort(!isShowSort)}
+            >
+              Sort by
+            </button>
+            {isShowSort && (
+              <div className="border p-5">
+                {optionSort.map((option, index) => (
+                  <div
+                    key={index}
+                    className={`${index < optionSort.length - 1 && "mb-5"} flex items-center`}
+                  >
+                    <input
+                      type="radio"
+                      value={option.value}
+                      className="mr-4 size-6 checked:bg-[#C1CD00]"
+                    />
+                    <span>{option.label}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,22 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      'xs': ['12px', {lineHeight: '16px'}],
+      'sm': ['14px', {lineHeight: '20px'}],
+      'base': ['16px', {lineHeight: '20px'}],
+      'lg': ['18px', {lineHeight: '24px'}],
+      'xl': ['24px', {lineHeight: '32px'}],
+      '2xl': ['32px', {lineHeight: '48px'}],
+      '3xl': ['40px', {lineHeight: '60px'}],
+      '4xl': ['48px', {lineHeight: '72px'}],
+      '5xl': ['64px', {lineHeight: '92px'}],
+      '6xl': ['96px', {lineHeight: '116px'}],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
