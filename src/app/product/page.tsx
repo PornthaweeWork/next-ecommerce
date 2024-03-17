e164700eb2484b3f75bb5fdbf2d8b64b7eb7e6b4
PornthaweeWork <pornthawee.work@gmail.com>
2024-03-18 01:49:27 +0700

    feat: create Accordion

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -2,9 +2,11 @@
 import React, { useState, useEffect } from "react";
 import SelectOption from "@/components/SelectOption";
 import Radio from "@/components/base/Radio";
+import Accordion from "@/components/Accordion";
 
 export default function ProductPage() {
   const [optionSelected, setOptionSelected] = useState("ASC");
+  const [accordionSelected, setAccordingSelected] = useState("T-Shirts1")
   const optionSort: { value: string; label: string }[] = [
     {
       value: "ASC",
@@ -20,8 +22,30 @@ export default function ProductPage() {
     },
   ];
 
+  const items: { id: number, title: string; contents: {value: string, label: string}[]}[] = [
+    {
+      id: 1,
+      title: 'หัวข้อ 1',
+      contents: [{value:'All', label:'All items'}, {value:'T-Shirts', label:'T-Shirts'}, {value:'Cardigans', label:'Cardigans'}],
+    },
+    {
+      id: 2,
+      title: 'หัวข้อ 2',
+      contents: [{value:'All1', label:'All items'}, {value:'T-Shirts1', label:'T-Shirts'}, {value:'Cardigans1', label:'Cardigans'}],
+    },
+    {
+      id: 3,
+      title: 'หัวข้อ 3',
+      contents: [{value:'All2', label:'All items'}, {value:'T-Shirts2', label:'T-Shirts'}, {value:'Cardigans2', label:'Cardigans'}],
+    },
+  ];
+
   const handleChangeOption = (value: string) => {
     setOptionSelected(value);
+  }
+
+  const handleChangeAccordion = (value: string) => {
+    setAccordingSelected(value);
   }
 
   useEffect(() => {
@@ -30,10 +54,14 @@ export default function ProductPage() {
 
   return (
     <div className="my-[100px] flex">
-      <div className="mr-[130px] w-[280px]">accdiron</div>
+      <div className="mr-[130px] w-[280px]">
+        {items.map((item, index) => (
+          <Accordion key={index} id={item.id} title={item.title} contents={item.contents} defaultSelected={accordionSelected} defaultExpand={2} onChange={handleChangeAccordion}/>
+        ))}
+      </div>
       <div className="flex flex-1">
-        <div className="flex w-full items-center justify-between">
-          <span className="self-start text-2xl font-bold">
+        <div className="flex w-full items-start justify-between">
+          <span className="text-2xl font-bold">
             Woman’s Clothing
           </span>
           <SelectOption titleButton="Sort By">
